301bbb0d10e7457c0605b09a8cd5e2527462012f
gabxii <apilgem@gmail.com>
2023-08-12 08:59:23 +0800

    fix mapbox sdk in gradle, add mapbox map view

--- app/src/main/java/com/android/spree_app/MainActivity.java
+++ app/src/main/java/com/android/spree_app/MainActivity.java
@@ -3,12 +3,20 @@ package com.android.spree_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.mapView);
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
     }
 }
